103900226d5c413b8116c982b77a114f92e47be6
Enoch Abiodun <g00380316@atu.ie>
2023-11-24 01:37:20 +0000

    Small changes

--- my-todolist/src/components/navbar/nav.js
+++ my-todolist/src/components/navbar/nav.js
@@ -4,12 +4,18 @@ import styles from '@/components/navbar/nav.module.css';
 
 export default function Nav() {
     return (
-        <nav className={styles.nav}>
+        <section className={styles.section}>
+            <nav className={styles.nav}>
             <img src="/logo.jpg"/>
             <Link href='/'><h1>YOURTDLIST</h1></Link>
             <Link href='/login'><p>Login</p></Link>
             <Link href='/task'><p>Tasks</p></Link>
             <Link href='/calender'><p>Calender</p></Link>
-        </nav>
+            </nav>
+            <div>
+                <h1>Starting making Tasks today</h1>
+            </div>
+        </section>
+        
     )
 }
